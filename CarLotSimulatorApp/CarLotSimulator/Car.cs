c4e4211d452d537390dcd11dd529afee00beb141
Tetyana Murzina <tetyanamur@gmail.com>
2022-08-02 19:19:23 -0500

    initial commit

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarLotSimulator
+{
+    public class Car
+    {
+        public Car()
+        {
+
+        }
+        public Car (int year, string make, string model, string engineNoise, string honkNoise, bool isDriveable)
+        {
+            Year = year;
+            Make = make;
+            Model = model;
+            EngineNoise = engineNoise;
+            HonkNoise = honkNoise;
+            IsDriveable = isDriveable;
+
+        }
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDriveable { get; set; }
+    }
+    
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace CarLotSimulator
 {
     class Program
@@ -7,20 +7,35 @@ namespace CarLotSimulator
         static void Main(string[] args)
         {
             //TODO
-
+            var Lexus = new Car(2019, "Lexus" ,"Eskridge", "Room", "Beep" , true);
+            var Hyndai = new Car(2015, "Hyndai", "Elantra", "Bzzzz", "Boom", true);
+            var Nissan = new Car(2018, "Nissan", "Altima", "Broozh", "Meep",  true);
             //Create a seperate class file called Car
             //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
             //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
-            //The methods should take one string parameter: the respective noise property
 
+            //* METHODS PT1:Lexus.MakeEngineNoise("Room");
+            //           Hyndai.MakeEngineNoise("Brzz");
+            //           Nissan.MakeEngineNoise("Broozh");
+            //
+            //PT2       Lexus.MakeHonkNoise("Beep");
+            //          MakeHonkNoise("Boom");
+            //          MakeHonkNoise("Meep");
+           
+            //The methods should take one string parameter: the respective noise property
+            var carList = new List<Car>() { Lexus, Hyndai, Nissan };
+
+                foreach (var vechile in carList)
+                {
+                Console.WriteLine($"{vechile.Year} {vechile.Make} {vechile.Model} {vechile.EngineNoise} {vechile.HonkNoise} {vechile.IsDriveable}");
+                }
 
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
             //Call each of the methods for each car
 
             //*************BONUS*************//
-
-            // Set the properties utilizing the 3 different ways we learned about, one way for each car
+          // Set the properties utilizing the 3 different ways we learned about, one way for each car
 
             //*************BONUS X 2*************//
 
